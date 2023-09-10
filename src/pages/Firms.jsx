76de93bbcd1bad940a7805ec6d717a1c60c47e2b
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-09-10 16:48:01 +0300

    dataAxios

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { Button, Typography } from "@mui/material";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Firms = () => {
-  return (
-    <div>Firms</div>
-  )
-}
+  const { token } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const getFirms = async () => {
+    const BASE_URL = "https://12256.fullstack.clarusway.com/";
+    const { data } = await axios(`${BASE_URL}stock/firms/`, {
+      headers: { Authorization: `Token ${token}` },
+    });
 
-export default Firms
+    dispatch(getSuccess());
+  };
+
+  useEffect(() => {
+    getFirms();
+  }, []);
+
+  return (
+    <div>
+      <Typography variant="h4" color="error" mb={3}>
+        Firm
+      </Typography>
+      <Button variant="contained"> New Firm</Button>
+    </div>
+  );
+};
+
+export default Firms;
