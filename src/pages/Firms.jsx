253a9a16e293bb1a0ec15ab290f58d3d1607186c
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-09-10 22:38:15 +0300

    try&catch

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -2,7 +2,7 @@ import { Button, Typography } from "@mui/material";
 import axios from "axios";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
+import {fetchFail,getSuccess,fetchStart} from "../features/stockSlice"
 const Firms = () => {
   const { token } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
@@ -10,12 +10,22 @@ const Firms = () => {
 
     
     const BASE_URL = "https://12256.fullstack.clarusway.com/";
-    const { data } = await axios(`${BASE_URL}stock/firms/`, {
-      headers: { Authorization: `Token ${token}` },
-    });
 
-    dispatch(getSuccess());
-  };
+    try {
+      dispatch(fetchStart())
+      const { data } = await axios(`${BASE_URL}stock/firms/`, {
+        headers: { Authorization: `Token ${token}` },
+      });
+  
+      dispatch(getSuccess())
+    
+      
+    } catch (error) {
+      console.log(error)
+      dispatch(fetchFail)
+      
+    }
+  }
 
   useEffect(() => {
     getFirms();
