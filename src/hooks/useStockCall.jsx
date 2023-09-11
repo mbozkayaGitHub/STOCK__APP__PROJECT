7bd5397008c7434fbb3e6dec363a15abc34fcd0e
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-09-11 23:19:41 +0300

    getFirms

--- src/hooks/useStockCall.jsx
+++ src/hooks/useStockCall.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
 
 const useStockCall = () => {
+
+    const getFirms = async () => {
+        const BASE_URL = "https://12256.fullstack.clarusway.com/";
+    
+        const url = "firms";
+    
+        try {
+          dispatch(fetchStart());
+          const { data } = await axios(`${BASE_URL}stock/firms/`, {
+            headers: { Authorization: `Token ${token}` },
+          });
+    
+          dispatch(getSuccess({ data, url }));
+        } catch (error) {
+          console.log(error);
+          dispatch(fetchFail);
+        }
+      };
+
+
   return (
     <div>useStockCall</div>
   )
