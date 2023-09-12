16081555d9ece4bb3004d2b2380eb2ebd23093b3
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-09-12 08:12:27 +0300

    useAxios

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,15 +1,24 @@
 import axios from "axios";
-
+import { useSelector } from "react-redux";
 
 
 const useAxios = () => {
+  const {token} = useSelector((state) =>state.auth)
+
+  const axiosPublic = axios.create({
+    baseURL: "https://12256.fullstack.clarusway.com/",
+    headers: { "Authorization": `Token ${ token}`}
+  })
   const axiosWithToken = axios.create({
     baseURL: "https://12256.fullstack.clarusway.com/",
-    timeout: 1000,
-    headers: { "X-Custom-Header": "foobar" },
-  });
+    headers: { "Authorization": `Token ${ token}`}
+  })
 
-  return <div>useAxios</div>;
+  return {axiosWithToken,axiosPublic}
 };
 
 export default useAxios;
+
+
+
+//Token : Token c5923a4a781411ed638a36e131ce62376c989f99
