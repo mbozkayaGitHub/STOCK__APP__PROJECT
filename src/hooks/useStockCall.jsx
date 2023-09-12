b58c76825556117578cb2911f380ea8ddd5c9634
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-09-12 06:49:37 +0300

    useStockCall

--- src/hooks/useStockCall.jsx
+++ src/hooks/useStockCall.jsx
@@ -1,33 +1,44 @@
-
 import { fetchFail, getSuccess, fetchStart } from "../features/stockSlice";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 
 const useStockCall = () => {
   const dispatch = useDispatch();
-  const { token } = useSelector((state) => state.auth)
+  const { token } = useSelector((state) => state.auth);
 
   const getStockData = async (url) => {
     const BASE_URL = "https://12256.fullstack.clarusway.com/";
-    dispatch(fetchStart())
+    dispatch(fetchStart());
 
     try {
-    
       const { data } = await axios(`${BASE_URL}stock/${url}/`, {
         headers: { Authorization: `Token ${token}` },
       });
 
-      dispatch(getSuccess({ data, url }));
+ getStockData(url)
     } catch (error) {
       console.log(error);
       dispatch(fetchFail);
     }
   };
 
-  return { getStockData };
+  const deleteStockData = async (url, id) => {
+    const BASE_URL = "https://12256.fullstack.clarusway.com/";
+    dispatch(fetchStart());
+
+    try {
+      await axios.delete(`${BASE_URL}stock/${url}/${id}/`, {
+        headers: { Authorization: `Token ${token}` },
+      });
+
+      dispatch(getSuccess({ url }));
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail);
+    }
+  };
+
+  return { getStockData,deleteStockData }
 };
 
 export default useStockCall;
-
-
-
