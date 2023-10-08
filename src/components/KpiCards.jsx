5786d9bc246ec6e09177cc8128248618e0274291
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-10-08 20:13:27 +0300

    styling

--- src/components/KpiCards.jsx
+++ src/components/KpiCards.jsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Avatar, Box, Grid, Paper, Typography } from "@mui/material";
 import React from "react";
 import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
@@ -6,25 +6,44 @@ import PaymentsIcon from "@mui/icons-material/Payments";
 import { amber, deepPurple, pink } from "@mui/material/colors";
 
 const KpiCards = () => {
+
+
   const data = [
     { id: 1, title: "sales", 
-    value: "$40000", icon: <MonetizationOnIcon />,
-    olor:deepPurple[600],
+    value: "$40000", icon: <MonetizationOnIcon  sx={{fontSize:"2.3rem"}}/>,
+    color:deepPurple[600],
     bgColor:deepPurple[100] },
     { id: 2, title: "profit", 
-    value: "$30000", icon: <ShoppingCartIcon />,
-    olor:pink[600],
+    value: "$30000", icon: <ShoppingCartIcon sx={{fontSize:"2.3rem"}}/>,
+    color:pink[600],
     bgColor:pink[100] },
     { id: 3, title: "purchases", 
-    value: "$10000", icon: <PaymentsIcon />,
-    olor:amber[600],
+    value: "$10000", icon: <PaymentsIcon  sx={{fontSize:"2.3rem"}}/>,
+    color:amber[600],
     bgColor:amber[100] },
   ];
 
   return (
-    <Grid container>
-      {data.map()}
-      <Grid item> </Grid>
+    <Grid container justifyContent={"center"} spacing={3}>
+      {data.map((item) => (
+        <Grid item key={item.id} xs={12} sm={10} md={6} lg={4}>
+            <Paper  sx={{p:2}}  elevation={10}> 
+                <Box sx={{display:"flex",alignItems:"center", gap:3}}>
+                 <Avatar sx={{backgroundColor:item.bgColor,color:item.color, width:60,height:60}}
+                 
+
+                 > 
+                    {item.icon}
+                    </Avatar>          
+              <Box> 
+                <Typography variant="button">{item.title}</Typography>
+                <Typography variant="h4">{item.value}</Typography>
+              </Box>
+                </Box>
+            </Paper>
+             </Grid>
+       ))}
+      
     </Grid>
   );
 };
